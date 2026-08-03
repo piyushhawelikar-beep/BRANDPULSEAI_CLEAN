bc531a16430d7980c725fde22802fb3034021732
Piyush Dhanraj Hawelikar <232564466+piyushhawelikar-beep@users.noreply.github.com>
2026-08-03 23:40:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-
-
 import streamlit as st
 import joblib
 import pickle
@@ -10,6 +8,8 @@ import pandas as pd
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 nltk.download("stopwords")
 nltk.download("wordnet")
 from tensorflow.keras.models import load_model
